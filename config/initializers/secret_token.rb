c47701b7ea0968acb2a854e97db0243da65b45ce
Antonio Carlos da Graça Mota Durão de Souza <ac.desouza@gmail.com>
2011-12-13 19:12:57 -0200

    Commit inicial, porque eu não consigo pensar em outra mensagem #mimimi.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rabugento::Application.config.secret_token = 'eb235e2eadcf18068f03827c5ce0c272877614c7d711da56df4c73c5ea061dfdb6fbbfb25e24929d85a21add5fe42dd802ffc4b10443abc7ee055933ebe263db'
