b6c82ca8710426cc5e936eba0ce741a201d4bceb
tinogomes <tinorj@gmail.com>
2011-12-14 18:43:42 -0200

    clean code at mimimis controller and added auto link to tweet

--- app/controllers/mimimis_controller.rb
+++ app/controllers/mimimis_controller.rb
@@ -1,21 +1,26 @@
 class MimimisController < ApplicationController
   def mimimis
-    render partial: 'mimimis', collection: Twitter.mimimis, as: 'mimimi'
+    render_mimimis_for Twitter.mimimis
   end
   
   def vdms
-    render partial: 'vdms', collection: Twitter.vdms, as: 'vdm'
+    render_mimimis_for Twitter.vdms
   end
   
   def affs
-    render partial: 'affs', collection: Twitter.affs, as: 'aff'
+    render_mimimis_for Twitter.affs
   end
   
   def fails
-    render partial: 'fails', collection: Twitter.fails, as: 'fail'
+    render_mimimis_for Twitter.fails
   end
   
   def porras
-    render partial: 'porras', collection: Twitter.porras, as: 'porra'
+    render_mimimis_for Twitter.porras
+  end
+  
+  private
+  def render_mimimis_for(collection)
+    render :partial => 'tweets', :collection => collection, :as => "tweet"
   end
 end
