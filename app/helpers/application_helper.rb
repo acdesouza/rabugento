853463f65f830bc75f6342f127e2d584bc3cce52
tinogomes <tinorj@gmail.com>
2011-12-16 16:09:28 -0200

    Adjustments on layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
+# encoding: utf-8
 module ApplicationHelper
+  def title(value)
+    @title = value
+  end
+  
+  def description(value)
+    @description = value
+  end
+  
+  def meta_description
+    tag :meta, :name => "description", 
+               :content => @description || "Rabugento, o site que monitora os todos os mimimis e agrega em um só lugar."
+  end
 end
